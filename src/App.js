e6fd9e35b78d5219d0144113b80d933dea7c21a8
shaqayq <shaqayq.darwazi@gmail.com>
2023-07-11 16:46:15 +0430

    create main component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import Navbar from "./layouts/Navbar"
+import Main from "./components/jobs/main";
+
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Main />
     </div>
   );
 }
