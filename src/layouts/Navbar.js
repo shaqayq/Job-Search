59d300d18088b17ac907d21e4d3abfbc2a160bf0
shaqayq <shaqayq.darwazi@gmail.com>
2023-07-11 15:54:13 +0430

    locate navbars item

--- src/layouts/Navbar.js
+++ src/layouts/Navbar.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import '../style/navbar.css'
-
+// import {FaGithub , FaLinkedinIn , FaTwitter } from 'react-icons/fa'
+import logo from '../img/logo.png'
 export default function Navbar() {
   return (
     <>
         <div className='navbar'>
-            <div id='logo'>
-                <h1>Find Work</h1>
+            <div>
+                <a href='#'><img src={logo} alt='logo' id='logo'/></a>
             </div>
             <div id='social'>
+           <p>About us</p>
+           <p>Your Jobs</p>
+           <p>Contact</p>
             
+
             </div>
         </div>
     </>
