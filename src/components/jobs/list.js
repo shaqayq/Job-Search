a5d3c76a20f01759f70fb7ce08146d5f16122a7f
shaqayq <shaqayq.darwazi@gmail.com>
2023-07-23 21:47:05 +0430

    create paginatin

--- src/components/jobs/list.js
+++ src/components/jobs/list.js
@@ -1,9 +1,14 @@
-import React , {useEffect} from 'react'
+import React , {useEffect , useState} from 'react'
 import {useDispatch , useSelector} from 'react-redux'
+
 import {fetchJobs} from '../../store/jobsReducer'
 import JobCard from './jobCard'
+import Pagination from '../Pagination'
 
 export default function List() {
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [jobsPerPage] = useState(8);
 
   const dispatch = useDispatch();
   const jobsList = useSelector((state)=>state.jobs)
@@ -12,17 +17,30 @@ export default function List() {
     dispatch(fetchJobs())
   },[] )
 
-  //  console.log(jobsList.length);
+  // Get current posts
+  const indexOfLastJob = currentPage * jobsPerPage;
+  const indexOfFirstJob = indexOfLastJob - jobsPerPage;
+  const currentJobs = jobsList.slice(indexOfFirstJob, indexOfLastJob);
+
+  // Change page
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   return (
    <>
     <div className='job-lists'>
     <div className='lists'>
-   {  jobsList.map((element) => {
+   {  currentJobs.map((element) => {
        return <JobCard key={element.id} details={element}/>
      })}
       </div>
+      <Pagination
+        jobsPerPage={jobsPerPage}
+        totalJobs={jobsList.length}
+        paginate={paginate}
+        currentNumber={currentPage}
+      />
     </div>
+    
    </>
   )
 }
