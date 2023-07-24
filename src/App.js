2e0bda4e2c128434e94282b7b7af2291bf6da419
shaqayq <shaqayq.darwazi@gmail.com>
2023-07-24 14:18:20 +0430

    add routes in App.js file

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import Navbar from "./layouts/Navbar"
 import Main from "./components/jobs/main";
+import About from './components/About'
 import Footer from './layouts/footer';
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <Navbar />
-      <Main />
+     <Routes exact path='/' element={<Main/>}/>
+     <Route element={<About/>}/>
+     
       <Footer/>
     </div>
+    </Router>
   );
 }
 
