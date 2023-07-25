29d34dd08e5f87799510b1026468810dfbd45d4e
shaqayq <shaqayq.darwazi@gmail.com>
2023-07-25 15:21:32 +0430

    create contact page

---
+++ src/components/Contact.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import '../style/contact.css'
+import contact from '../img/contact.png'
+export default function Contact() {
+  return (
+    <>
+        <div class="container">
+      <div class="content">
+        <div class="image-box">
+          <img src={contact} alt="" />
+        </div>
+        <form action="#">
+          <div class="topic">Send us a message</div>
+          <div class="input-box">
+            <input type="text" required />
+            <label>Enter your name</label>
+          </div>
+          <div class="input-box">
+            <input type="text" required />
+            <label>Enter your email</label>
+          </div>
+          <div class="message-box">
+            <textarea></textarea>
+            <label>Enter your message</label>
+          </div>
+          <div class="input-box">
+            <input type="submit" value="Send Message" />
+          </div>
+        </form>
+      </div>
+    </div>
+
+    </>
+  )
+}
